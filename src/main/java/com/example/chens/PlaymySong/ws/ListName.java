61d43d8b7134a5069e56683a2f8d5ee21bf9e1e4
liangxt <liangxt07@gmail.com>
2017-03-14 22:27:24 -0400

    Update ListName.java

--- src/main/java/com/example/chens/PlaymySong/ws/ListName.java
+++ src/main/java/com/example/chens/PlaymySong/ws/ListName.java
@@ -1,8 +1,6 @@
 package com.example.chens.PlaymySong.ws;
 
-/**
- * Created by Songze Chen on 2016/4/15.
- */
+
 public interface ListName {
     public static final String FAVORITELIST = "FavoriteList";
     public static final String PLAYLIST = "PlayList";
